5fdec1fb671fa421aec3d3588afe9a88a116ec8b
TranHien94 <tranhien7633@gmail.com>
2023-07-01 12:50:50 +0700

    ad code

--- src/Components/manageProduct/ListProduct.jsx
+++ src/Components/manageProduct/ListProduct.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 import Product from './Product'
 import '../scss/ShoppingCard.scss'
 
 export default function ListProduct() {
     const listProducts= useSelector((state)=>state.listProduct)
+    const [filterTitle, setFilterTitle]=useState('')
+    const filterProduct =listProducts.filter((product)=>product.productName.toLowerCase().includes(filterTitle.toLowerCase()))
   return (
     <>
+    
           <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+            
               <div className="panel panel-primary">
                   <div className="panel-heading">
+                    
                       <h1 className="panel-title">List Products</h1>
+                      <input className="panel-title container"
+                        type='text' placeholder='Search Product...'
+                          value={filterTitle}
+                          onInput={(e) => { setFilterTitle(e.target.value) }}></input>
+                      
                   </div>
+                  
                   <div className="panel-body" id="list-product">
                     
-                      {listProducts.map((product) => (
+                      {filterProduct.map((product) => (
                           <Product key={product.productId} product={product} />
                       ))}
                      
